5a1fe41d9ce79229585269f0e13b342e27973f8e
1211186431 <56494120+1211186431@users.noreply.github.com>
2020-09-09 11:13:49 +0800

    9.9.2

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent =getIntent();
+        Button button=(Button)findViewById(R.id.button2);
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age",10);
+        Toast.makeText(this,name+" "+age,Toast.LENGTH_LONG).show();
     }
 }
