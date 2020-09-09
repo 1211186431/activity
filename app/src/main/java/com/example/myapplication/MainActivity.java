17477a471f4c24cdd0222c93d637af8456a55a34
1211186431 <56494120+1211186431@users.noreply.github.com>
2020-09-09 13:08:02 +0800

    9.9.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +21,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=
                 new Intent(MainActivity.this,MainActivity2.class);
-                intent.putExtra("name","Zhangsan");
+                EditText editText=findViewById(R.id.textView1);
+                String name=editText.getText().toString();
+                intent.putExtra("name",name);
                 intent.putExtra("age",20);
-                startActivity(intent);
+                startActivityForResult(intent,0);
+            }
+        });
+
+        Button button2=(Button)findViewById(R.id.b2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=
+                        new Intent(MainActivity.this,MainActivity3.class);
+                EditText editText=findViewById(R.id.textView1);
+                String name=editText.getText().toString();
+                intent.putExtra("name",name);
+                intent.putExtra("age",20);
+                startActivityForResult(intent,0);
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==0 && resultCode==0){
+            String str=data.getStringExtra("result");
+            Toast.makeText(this,str,Toast.LENGTH_LONG).show();
+        }
+        if(requestCode==0 && resultCode==1){
+            String str=data.getStringExtra("result2");
+            Toast.makeText(this,str,Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -6,24 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
         Intent intent =getIntent();
-        Button button=(Button)findViewById(R.id.button2);
+        Button button=(Button)findViewById(R.id.b4);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=getIntent();
                 String name=intent.getStringExtra("name");
-                Integer age=intent.getIntExtra("age", 20);
-                intent.putExtra("result","姓名:"+name+" 年龄"+age);
-                setResult(0,intent);
+                intent.putExtra("result2","姓名："+name);
+                setResult(1,intent);
                 finish();
             }
         });
